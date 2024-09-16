3cacb5cdb67b3d2b485f86e02fc47798bb8e3e6e
selvasekar <133844229+selvasekar532@users.noreply.github.com>
2024-09-16 22:22:55 +0530

    Add files via upload

---
+++ Monday_16th_9_1st_file.sql
@@ -0,0 +1,16 @@
+create database stud
+use stud
+
+create table student(
+rollno numeric(5),
+sname varchar(20),
+age int,
+address varchar(30),
+phone_no varchar(10)
+
+)
+select * from student
+
+insert into student values(19228,'Kisan',18,'Madurai',8056697801),
+insert into student values(19244,'Vetri',18,'Madurai',8072663057)
+
